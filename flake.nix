da205b6adab1f975751d700aa37253c6409a4f46
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2023-07-27 15:18:02 -0700

    chore: Setup devenv

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      devShells = forEachSystem
+        (system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+          {
+            default = devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [
+                {
+                  # https://devenv.sh/reference/options/
+                  packages = with pkgs; [
+                    just
+                    vale
+                    nodejs
+                    hunspellDicts.en-us-large
+                  ];
+                  env.DICPATH = "${pkgs.hunspellDicts.en-us-large}/share/hunspell";
+                }
+              ];
+            };
+          });
+    };
+}
