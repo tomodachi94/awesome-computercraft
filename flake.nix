72cb544251fac9f2ac56b6f0d9bd41a26631f146
Tomodachi94 <tomodachi94@protonmail.com>
2024-06-08 15:46:55 -0700

    flake.nix: include lychee in shell

--- flake.nix
+++ flake.nix
@@ -24,6 +24,7 @@
                   # https://devenv.sh/reference/options/
                   packages = with pkgs; [
                     just
+                    lychee
                     vale
                     tomodachi94.packages.${system}.awesome-lint
                     hunspellDicts.en-us-large
