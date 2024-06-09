fa037f59f9ed607da33835cc2e09a0cbc7a0fa4b
Tomodachi94 <tomodachi94@protonmail.com>
2024-06-08 17:29:37 -0700

    flake.nix: use nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
     tomodachi94.url = "github:tomodachi94/dotfiles?dir=pkgs";
   };
